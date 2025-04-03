5bfab1e8323b064410096ebd53bddaf7c92ba096
fedebacci <federicobacci1999@gmail.com>
2025-04-04 00:02:15 +0200

    separazione delle due consegne e modifica nomi variabili

--- 03-array-bonus-samuel/script.js
+++ 03-array-bonus-samuel/script.js
@@ -8,32 +8,34 @@
 // 3 CONSEGNA: PARTI DA CONSEGNA 2 E TORNA A CONSEGNA 1 (DECOMPRIMI STRINGA)
 
 
-let initialString = "pprrroooovvvvvaaaaaap";
-let finalString = "";
-let stringToDecode = "";
-// console.log("initialString:", initialString);
+let initialString = "pprrroooovvvvvaaaaaavvvvvoooorrrpp";
+let compressedString = "";
 for (let i = 0; i < initialString.length; i++) {
     const currentLetter = initialString[i];
     const previousLetter = initialString[i - 1];
 
     if (previousLetter === undefined || currentLetter !== previousLetter) {
-        // console.log("PRIMA LETTERA O LETTERA CAMBIATA:", currentLetter);
-        finalString += currentLetter;
-        stringToDecode += currentLetter + 1;
-    } else if (currentLetter === previousLetter) {
-        // console.info("UGUALI");
-        // console.log("stringToDecode", stringToDecode);
-
-        const oldLastCharacter = stringToDecode[stringToDecode.length - 1]
-        // console.log("oldLastCharacter", oldLastCharacter)
-
-        const newLastCharacter = parseInt(oldLastCharacter) + 1
-        // console.log("newLastCharacter", newLastCharacter);
-
-        stringToDecode = stringToDecode.slice(0, -1) + newLastCharacter;
+        compressedString += currentLetter;
     }
     
 }
-console.info("initialString:", initialString);
-console.info("finalString:", finalString);
-console.info("stringToDecode:", stringToDecode);
+console.info("1) initialString:", initialString);
+console.info("1) compressedString:", compressedString);
+
+
+
+
+let compressedStringWithLetterCount = "";
+for (let i = 0; i < initialString.length; i++) {
+    const currentLetter = initialString[i];
+    const previousLetter = initialString[i - 1];
+
+    if (previousLetter === undefined || currentLetter !== previousLetter) {
+        compressedStringWithLetterCount += currentLetter + 1;
+    } else if (currentLetter === previousLetter) {
+        const oldLastCharacter = compressedStringWithLetterCount[compressedStringWithLetterCount.length - 1]
+        const newLastCharacter = parseInt(oldLastCharacter) + 1
+        compressedStringWithLetterCount = compressedStringWithLetterCount.slice(0, -1) + newLastCharacter;
+    }
+}
+console.info("2) compressedStringWithLetterCount:", compressedStringWithLetterCount);
